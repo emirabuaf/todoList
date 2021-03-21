f0325b9a490875b5e9e6c942cc547686f1898a40
emir abuaf <emir@made-for.com>
2021-03-21 11:31:22 +0100

    render todos

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,28 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState({});
 
   useEffect(() => {
     fetch("api/tasks")
       .then((response) => response.json())
       .then((data) => {
         setTasks(data);
-        console.log(data);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  return (
-    <div className="App">
-      <h1>app</h1>
-    </div>
-  );
+  const renderTasks = () => {
+    let newTasksArray = [];
+    for (var prop in tasks) {
+      newTasksArray.push(tasks[prop]);
+    }
+    return newTasksArray.map((newTask) => <h1>{newTask.text}</h1>);
+  };
+
+  return <div className="App">{renderTasks()}</div>;
 }
 
 export default App;
