505a18b03f969027a706bf5614afa1565aaf8700
emir abuaf <emir@made-for.com>
2021-03-22 14:22:40 +0100

    send request when delete item

--- client/src/components/DeleteTodo/index.js
+++ client/src/components/DeleteTodo/index.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DeleteTodo = ({ tasks, setTasks, id }) => {
   const handleDelete = (id) => {
     const newList = tasks.filter((item) => item.id !== id);
     setTasks(newList);
+    fetch(`/api/tasks/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => console.log("deleted"))
+      .catch((error) => console.log(error));
   };
   return (
     <div>
